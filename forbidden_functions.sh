4376f0fe97b90fa189196e33531c33638dbc03d9
AfonsoMota-132 <afonsomota88@gmail.com>
2024-12-05 21:35:29 +0000

    added support for libft and printf, and filtered the results to show only when forbidden functions are used, and which ones are used

--- forbidden_functions.sh
+++ forbidden_functions.sh
@@ -11,11 +11,25 @@
 # **************************************************************************** #
 
 #!bin/bash
-libft="malloc"
 
-if [ "$1" = "push_swap" ]; then
+libft="malloc\|free\|write"
+
+if [ "$1" = "libft.a" ]; then
+	POSSIBLE="malloc\|free\|write"
+elif [ "$1" = "libftprintf.a" ]; then
+	POSSIBLE="$libft\|va_start\|va_arg\|va_copy\|va_end"
+elif [ "$1" = "push_swap" ]; then
 	POSSIBLE="exit\|free\|$libft"
 fi
 
-nm $1 -u | grep -v "__"\
-	| awk '{print $2}' | awk -F "@" '{print $1}' | grep -v $POSSIBLE
+
+if [ $( nm $1 | grep "U" | grep -v "__" | \
+	grep -v "ft" |awk '{print $2}' |\
+	awk -F "@" '{print $1}' | grep -v $POSSIBLE | wc -l ) -ne 0 ]; then
+	echo "ERROR"
+	nm $1 | grep "U" | grep -v "__" | grep -v "ft" | awk '{print $2}' | \
+		awk -F "@" '{print $1}' | grep -v $POSSIBLE
+else
+	echo "No forbidden functions"
+fi
+
